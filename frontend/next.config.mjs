b3c4547c848ce03ca94d289b9a08451a3e75f51a
kaanshika759-a11y <kaanshika759@gmail.com>
2026-09-20 17:23:53 +0530

    final clean config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,10 +2,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  env: {
-    NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL ?? "http://localhost:4000"
-  },
-
   output: 'export',
   basePath: '/aaa',
 };
