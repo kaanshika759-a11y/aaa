4ca1d8ab6d8265f432303b898a133eb47ed42f8a
kaanshika759-a11y <kaanshika759@gmail.com>
2026-09-20 17:20:32 +0530

    r remove rewrites

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,18 +5,7 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL ?? "http://localhost:4000"
   },
-  async rewrites() {
-    const backendUrl =
-      process.env.NEXT_PUBLIC_BACKEND_URL ??
-      process.env.BACKEND_URL ??
-      "http://localhost:4000";
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${backendUrl}/api/:path*`,
-      },
-    ];
-  },
+
   output: 'export',
   basePath: '/aaa',
 };
